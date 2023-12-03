477e0b0944f26f80f23d8dcf892f4885acea38d6
Yibo Mu <muyibo@mail.ustc.edu.cn>
2023-12-03 16:31:46 +0800

    Update 12.3

--- ostep-homework/cpu-api/2.c
+++ ostep-homework/cpu-api/2.c
@@ -8,18 +8,26 @@
 
 int main()
 {
-    FILE *f = fopen("test_2.txt", "r+");
-    if(f == NULL){
+    FILE *fd = fopen("test_2.txt", "w+");
+    if(fd == NULL){
         fprintf(stderr, "open file failed\n");
         exit(1);
     }
-
+    
     int rc = fork();
     if(rc < 0){
         fprintf(stderr, "fork failed\n");
         exit(1);
     }
-    else if(rc == 0){
-        
+    else if(rc == 0){  // child process
+        const char *child_msg = "Child is writing to the file\n";
+        fputs(child_msg, fd);
     }
+    else{ // parent process
+        const char *parent_msg = "Parent is writing to the file\n";
+        fputs(parent_msg, fd);
+    }
+
+    fclose(fd);
+    return 0;
 }
